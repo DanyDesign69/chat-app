8aef69ad3be672502ef4df51697bb81a9de4e17e
danydesign69 <danyartzz6@gmail.com>
2024-12-23 19:02:55 +0100

    outra vez

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('Novo usuário conectado');
